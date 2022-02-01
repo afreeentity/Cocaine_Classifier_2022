1c20d08a8cb836818624afd5dc33151a263bf518
Mehrdad Hasanpour <89336514+afreeentity@users.noreply.github.com>
2022-02-01 10:23:05 +0100

    Initial_template

---
+++ main.py
@@ -0,0 +1,12 @@
+# System imports
+
+# Libraries
+
+# Internal
+from Utils.configs import parse_args, print_configs, configure
+
+parse_args()
+print_configs()
+configure(_)
+
+# Call some main functions here
